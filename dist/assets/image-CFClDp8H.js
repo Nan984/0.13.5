ef2b06f621d350c88a651bdf6c1defd2c4a8831b
Nan984 <aziz.salimov.26@mail.ru>
2026-06-29 13:15:08 +0500

    Updated index.html

--- dist/assets/image-CFClDp8H.js
+++ dist/assets/image-CFClDp8H.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-c77Kxe6W.js";/**
+import{c as e}from"./index-Cl8nCwDG.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
